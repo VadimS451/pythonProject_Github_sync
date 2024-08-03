ea6d043d7fb59a782dbd5607cd5ad8156408aaec
VadimS451 <vadim.shavlovsky@gmail.com>
2024-08-03 16:52:53 +0300

    версия 3 - правильно считает начало недели с нуля часов

--- CalendarHoursCounter.py
+++ CalendarHoursCounter.py
@@ -8,17 +8,25 @@ CALENDAR_ID = '3ea85527e406a2b94229ed0f2b70397fbe60f7fa4b1789e3a9c0fdb7546c1e3f@
 # Путь к файлу с ключом от сервисного аккаунта в Google Cloud
 CREDENTIALS_FILE = './calendarintegration-431411-9b6b001b20aa.json'
 
+
 def main():
-    credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=['https://www.googleapis.com/auth/calendar.readonly'])
+    credentials = (service_account.Credentials.from_service_account_file
+                   (CREDENTIALS_FILE, scopes=['https://www.googleapis.com/auth/calendar.readonly']))
     service = build('calendar', 'v3', credentials=credentials)
 
-    # Получить начало и конец текущей недели
+    # Получить начало и конец текущей недели с нуля часов
     now = datetime.now(timezone.utc)
-    start_of_week = now - timedelta(days=now.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
+    start_of_week = datetime(now.year, now.month, now.day, tzinfo=timezone.utc) - timedelta(days=now.weekday())
+    end_of_week = start_of_week + timedelta(days=6, hours=23, minutes=59, seconds=59)
 
     # Получить события из календаря
-    events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=start_of_week.isoformat(), timeMax=end_of_week.isoformat(), singleEvents=True, orderBy='startTime').execute()
+    events_result = service.events().list(
+        calendarId=CALENDAR_ID,
+        timeMin=start_of_week.isoformat(),
+        timeMax=end_of_week.isoformat(),
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
     events = events_result.get('items', [])
 
     total_duration = 0
@@ -29,7 +37,11 @@ def main():
         total_duration += duration.total_seconds()
 
     total_hours = total_duration / 3600
+    print('Вывод переменной "now"', now)
+    print('Начало недели:', start_of_week)
+    print('Конец недели:', end_of_week)
     print(f'Общее время событий на этой неделе: {total_hours:.2f} часов')
+
 
 if __name__ == '__main__':
     main()
